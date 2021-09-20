fc370efa2e26cfece4534539203cae2c98c8bac1
Antonio <71550295+le4nnt0nn@users.noreply.github.com>
2021-09-20 23:34:20 +0200

    Card.js : added Layout in wrapper

---
+++ fr-card/src/components/Card/Card.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Layout, Wrapper } from './Card.styles'
+
+export default class Card extends Component {
+    render() {
+        return (
+            <Wrapper>
+                <Layout/>
+            </Wrapper>
+        )
+    }
+}
