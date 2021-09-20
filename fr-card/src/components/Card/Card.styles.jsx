f77a65c3eea0639ae45f8c847409b731bc2e5b81
Antonio <71550295+le4nnt0nn@users.noreply.github.com>
2021-09-20 23:34:30 +0200

    Card.styles.jsx : added styles

---
+++ fr-card/src/components/Card/Card.styles.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+  display: flex;
+  text-align: center;
+  justify-content: center;
+`;
+
+export const Layout = styled.div`
+width: 370px;
+height: 467px;
+margin: 50px auto;
+
+background: #F5F5F5;
+box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+border-radius: 20px;
+`;
