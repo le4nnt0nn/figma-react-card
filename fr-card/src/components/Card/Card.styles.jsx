0cb16ae4de25ea30f70c38908182f54508c8b9ff
Antonio <71550295+le4nnt0nn@users.noreply.github.com>
2021-09-21 09:16:49 +0200

    Card.styles.jsx : created new sc

--- fr-card/src/components/Card/Card.styles.jsx
+++ fr-card/src/components/Card/Card.styles.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 export const Wrapper = styled.div`
   display: flex;
@@ -8,11 +8,19 @@ export const Wrapper = styled.div`
 `;
 
 export const Layout = styled.div`
-width: 370px;
-height: 467px;
-margin: 50px auto;
+  position: relative;
+  width: 370px;
+  height: 467px;
+  margin: 50px auto;
+  background: #f5f5f5;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 20px;
+`;
 
-background: #F5F5F5;
-box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-border-radius: 20px;
+export const Header = styled.div`
+  position: absolute;
+  width: 370px;
+  height: 137px;
+  background: linear-gradient(90deg, #ffe259 0%, #ffa751 100%);
+  border-radius: 20px;
 `;
