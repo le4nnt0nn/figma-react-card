4ceb8f1bf0fe28f70413ecee575d1e824d11b2bf
Antonio <71550295+le4nnt0nn@users.noreply.github.com>
2021-09-21 09:43:02 +0200

    Card.js : added Title and Subtitle

--- fr-card/src/components/Card/Card.js
+++ fr-card/src/components/Card/Card.js
@@ -1,14 +1,25 @@
 import React, { Component } from "react";
-import { Wrapper, Layout, Header, TextGroup, Title } from "./Card.styles";
+import {
+  Wrapper,
+  Layout,
+  Header,
+  TextGroup,
+  Title,
+  Subtitle,
+} from "./Card.styles";
 
 export default class Card extends Component {
   render() {
     return (
       <Wrapper>
         <Layout>
-          <Header/>
+          <Header />
           <TextGroup>
-              <Title>Trying Figma and React</Title>
+            <Title>Trying Figma and React</Title>
+            <Subtitle>
+              A test trying Figma and React.js with styled components. I want to
+              learn more about this. Looks cool!
+            </Subtitle>
           </TextGroup>
         </Layout>
       </Wrapper>
